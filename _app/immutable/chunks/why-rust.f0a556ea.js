ff926959896fa9c8252fdf12d13136e9756df033
Vonr <Vonr@users.noreply.github.com>
2023-09-20 10:01:50 +0000

    deploy: e382348d6726ce55b4355a2803e0e6c14121caeb

---
+++ _app/immutable/chunks/why-rust.f0a556ea.js
@@ -0,0 +1,330 @@
+const n=`# Why Rust?
+## Sep 20 2023
+
+I have used a variety of languages since I first started my foray into programming.
+
+I started with Javascript, learnt Java, hopped to Kotlin, pivoted to Go, and touched on a bunch of other languages along the way.
+
+Despite all my time with these other languages, I find myself routinely reaching for Rust
+for whatever I am working on.
+
+Rust was a language that I had a lot of trouble picking up.  
+I attempted learning it 8 times before it finally clicked.  
+What made it so different, and why do I enjoy it?
+
+## The Borrow Checker
+
+Rust has a system that it calls the borrow checker.  
+This program is at the core of Rust, upholding its borrowing rules.
+
+In Rust, when something is passed by value to another function, it is moved out of its original function.
+This means that you can no longer access it, even after the function is over.
+
+\`\`\`rs
+fn main() {
+    let s = String::from("Hello, world!");
+
+    print(s);
+
+    // This would be an error, as s has moved into \`print\`
+    println!("{s}")
+}
+
+fn print(s: String) {
+    println!("{s}")
+}
+\`\`\`
+[Playground](https://play.rust-lang.org/?version=stable&mode=debug&edition=2021&gist=4cd8bcc42a9e27c8a287a0295b68a362)
+
+Of course, this by itself is inconvenient.  
+<!-- That's why Rust makes heavy use of borrows - creating references to values. -->
+
+References are split into two distinct types: shared and immutable, and exclusive and mutable.
+
+Mutable references can not coexist with any other reference, regardless of mutability.
+
+As such, you must pick between having as many immutable references to an object as needed,
+or a single mutable reference.
+
+\`\`\`rs
+fn main() {
+    let s = String::from("Hello, world!");
+
+    // We give \`print\` a reference to \`s\` instead of \`s\` itself
+    print(&s);
+
+    // \`s\` has not been moved out of this function,
+    // and is thus still valid.
+    println!("{s}")
+}
+
+// \`print\` now takes in reference to a String.
+//
+// P.S. the type should really be &str since
+// we are only interested in the data of
+// the string.
+fn print(s: &String) {
+    println!("{s}")
+}
+\`\`\`
+[Playground](https://play.rust-lang.org/?version=stable&mode=debug&edition=2021&gist=d1c040196f4c5a11c920ad0c9a2874a6)
+
+This rule may seem quite arbitrary at first glance, but this simple rule prevents an entire class of bugs,
+such as use-after-free and double-free, which may be especially freeing for developers coming from a C or C++ background.
+
+The borrow checker also gives Rust the ability to know when a value can be freed
+statically at compile-time, making the use of a garbage collector unnecessary and
+allowing the programmer to not have to worry about memory leaks.
+
+That said, Rust also gives you the tools to free (or \`drop\`) a value manually as long as you have ownership of it.  
+In fact, it is such a simple function you could write it yourself - no compiler magic!
+
+\`\`\`rs
+pub fn drop<T>(_x: T) {}
+\`\`\`
+[Docs](https://doc.rust-lang.org/std/mem/fn.drop.html)
+
+If you wish to learn more about what the borrow checker does, I recommend reading  
+[this article](https://blog.logrocket.com/introducing-the-rust-borrow-checker/).
+
+## Enums (Sum Types)
+
+The borrow checker is really neat, but one of the biggest reasons I love Rust is how
+expressive the language is, in many ways thanks to its powerful enums.
+
+If you're familiar with algebraic type systems, Rust's enums are sum types,
+and those of you who know what that implies would already be excited about it.
+
+Let's take a look at the \`Option\` type.
+This type exists in the [standard library](https://doc.rust-lang.org/std/option/enum.Option.html)
+and is defined similarly to this.
+
+\`\`\`rs
+enum Option<T> {
+    Some(T),
+    None,
+}
+\`\`\`
+
+It replaces the \`null\`s and \`nil\`s of other languages and is analogous to the \`Maybe\` type in Haskell.
+
+The most obvious advantage of using a sum type to represent "nullable" values is that they are typed, unlike \`null\`s in
+a few other languages such as Java, where you can pass a \`null\` for any argument.
+
+In Rust, you would have to explicitly write that what you are expecting is an \`Option\`.  
+If you don't, you will simply get a compile error.
+
+\`\`\`rs
+fn foo(num: Option<i32>) -> Option<i32> {
+    num.map(|num| num * 2)
+}
+
+fn main() {
+    assert_eq!(foo(None), None);
+    assert_eq!(foo(Some(2)), Some(4));
+}
+\`\`\`
+[Playground](https://play.rust-lang.org/?version=stable&mode=debug&edition=2021&gist=89255fd5cb5ab72b62c8ff772e599acc)
+
+As shown in the above example, another advantage is that being a type, we can attach methods such as \`map\` onto it.
+
+This makes working with optional values more concise and convenient.
+
+Rust also has a \`Result\` type which is very similar to \`Option\`, replacing exceptions instead of \`null\`.
+You can read more about it [here](https://doc.rust-lang.org/std/result/enum.Result.html).
+
+## Traits (Typeclasses)
+
+If you've had experience with the ML family of langauges or Haskell, you would be familiar with the concept of typeclasses.
+
+Rust was influenced by Haskell's typeclasses, resulting in traits.
+
+Traits allow us to compartmentalize behaviour into interface-like structures.
+
+We can implement any number of traits for an enum or struct.  
+We can also implement foreign traits for our own types, or our own traits for foreign types.
+
+\`\`\`rs
+trait Legged {
+    fn legs(&self) -> u64;
+}
+
+struct Table {
+    legs: u64,
+}
+
+struct Chair;
+
+impl Legged for Table {
+    fn legs(&self) -> u64 {
+        self.legs
+    }
+}
+
+impl Legged for Chair {
+    fn legs(&self) -> u64 {
+        4
+    }
+}
+
+// This function takes in any L such that L implements Legged.
+fn has_more_than_3_legs<L: Legged>(legged: L) -> bool {
+    legged.legs() > 3
+}
+
+fn main() {
+    assert!(has_more_than_3_legs(Chair));
+    assert!(!has_more_than_3_legs(Table { legs: 3 }))
+}
+\`\`\`
+[Playground](https://play.rust-lang.org/?version=stable&mode=debug&edition=2021&gist=0c7470eeff0dd17b784d38d27eb75507)
+
+In the above example, the function \`has_more_than_3_legs\` has a simple generic bound, but typeclasses let us express
+much more complex bounds.
+
+\`\`\`rs
+use std::ops::Add;
+use std::fmt::Display;
+
+fn double_and_print<T: Add<Output = T> + Display + Clone>(val: T) {
+    println!("{}", val.clone() + val)
+}
+
+fn main() {
+    double_and_print(2); // prints "4"
+}
+\`\`\`
+[Playground](https://play.rust-lang.org/?version=stable&mode=debug&edition=2021&gist=ccf2621556e0fe53957be7c9f89938db)
+
+There are also alternative syntaxes to specify bounds.
+For longer bounds like that of \`double_and_print\`, we can use the \`where\` keyword to specify the bounds after the arguments.
+
+\`\`\`rs
+fn double_and_print<T>(val: T)
+where
+    T: Add<Output = T> + Display + Clone
+{
+    println!("{}", val.clone() + val)
+}
+\`\`\`
+
+For shorter bounds, we can use the \`impl\` keyword inside the arguments themselves, but this would prevent us from specifying
+the generic type(s) if type inference falls short.
+
+\`\`\`rs
+fn debug(val: impl Display) {
+    println!("{val}")
+}
+\`\`\`
+
+Rust has a lot of small conveniences like this, making it both flexible and pleasant to use.
+
+You can also use dynamic dispatch to make a "trait object" type.
+
+It should be noted that trait objects are unsized as different implementors of the trait may have different sizes.  
+As such, we put them on the heap using the \`Box\` smart pointer and store them in a heap-allocated \`Vec\`.
+
+\`\`\`rs
+use std::fmt::Debug;
+
+fn main() {
+    let vec: Vec<Box<dyn Debug>> = vec![
+        Box::new(1),
+        Box::new(Some(3)), 
+        Box::new("test")
+    ];
+    
+    println!("{vec:?}")
+}
+\`\`\`
+[Playground](https://play.rust-lang.org/?version=stable&mode=debug&edition=2021&gist=05af188547fe84ba81c0aae62f8e2373)
+
+## Iterators
+
+Iterators are by far my favourite feature of Rust, utilizing its powerful type system
+to create a concise and easy way of manipulating data.
+
+Most collection types in Rust either have an \`iter\` method or implement \`IntoIterator\`,
+which craetes an \`Iterator\` over them.
+
+Once in an \`Iterator\`, you get access to a wide variety of helper methods to manipulate it
+such as \`filter\`, \`map\`, \`skip\`, and \`take\` among many more.
+
+Rust's \`Iterator\` is "lazy", meaning that operations on them only run when needed.
+This avoids allocating a container for each operation, which would be necessary in a strict API.
+
+\`\`\`rs
+fn main() {
+    // This is a bad use of \`map\`, \`inspect\` is more suitable here.
+    for _ in (1..=10).map(|n| println!("{n}")).take(3) {
+        // Draining the iterator, as it would
+        // otherwise be lazy and do nothing.
+    }
+    // By the end of the loop,
+    // we'd have printed
+    // next 1
+    // next 2
+    // next 3
+}
+\`\`\`
+[Playground](https://play.rust-lang.org/?version=stable&mode=debug&edition=2021&gist=b9b5ff8b9c9b56e5bb6224efba7eae19)
+
+Notice how we only printed "next" three times, despite the \`inspect\` taking place before \`take\`.
+
+The \`Iterator\` we produced is equivalent to the following imperative code.
+
+\`\`\`rs
+let mut count = 0;
+for n in (1..=10) {
+    // Shadowing the binding of \`n\` to the result of the map.
+    let n = println!("{n}");
+
+    count += 1;
+    if count >= 3 {
+        break
+    }
+
+    // inner loop
+}
+\`\`\`
+
+While doing the same in Javascript would lead to the equivalent of the following instead,
+not only unexpectedly printing all 10 numbers, but also allocating two new arrays.
+
+\`\`\`js
+let mapped = [];
+
+for (n of [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) {
+    mapped.push(console.log(n));
+}
+
+let taken = mapped.slice(0, 3);
+
+for (n of taken) {
+    // inner loop
+}
+\`\`\`
+
+In my experience, I have never encountered a situation where I'd rather have a strict API here,
+and Rust's lack of container allocations makes it the better option for me.
+
+# Conclusion
+
+I have only discussed a small portion of the reasons that I love using Rust, but the
+reasons I have discussed are the primary reasons why I continue to choose Rust for
+many of my projects.
+
+With all that said, I believe it is worth nothing that Rust is neither a perfect langauge
+nor a language that should be used for everything.
+
+In fact, I considered writing this website in Rust for fun, but decided against it in the
+end as I would be losing out on the fantastic Javascript ecosystem which could help
+me push out this website faster.
+
+I believe that Rust deserves a spot on your toolbelt, so that you can easily
+harness it's powers when it fits the project. I personally make a lot of
+command-line utilities, which I believe Rust to be an excellent choice for.
+
+If you are interested in learning Rust, I strongly recommend reading the [Book](https://doc.rust-lang.org/book)
+or doing [Rustlings exercises](https://github.com/rust-lang/rustlings).
+`;export{n as default};
---
+++ _app/immutable/nodes/5.230c68b9.js
@@ -0,0 +1,36 @@
+import{s as oe,o as ce,n as pe}from"../chunks/scheduler.6c817c6f.js";import{S as le,i as ie,e as re,a as T,d as b,p as fe,t as I,b as ue,f as v,g as d,r as z,m as y,s as E,h as _,j as L,u as x,n as $,c as j,k as p,y as o,v as M,o as me,w as G,A as ge,x as V}from"../chunks/index.5c4c8a2d.js";import{F as he}from"../chunks/fa.66bbcc8c.js";import{d as de}from"../chunks/index.ac23d56d.js";import{f as _e}from"../chunks/index.04ca9c1a.js";function ne(u){let r,t,e,n,i,f,s;e=new he({props:{icon:_e,class:"inline text-black dark:text-white mr-0.5"}});let a=u[1]&&u[3].stargazers_count>0&&se(u);return{c(){r=d("span"),t=d("a"),z(e.$$.fragment),n=y(u[0]),f=E(),a&&a.c(),this.h()},l(l){r=_(l,"SPAN",{class:!0});var c=L(r);t=_(c,"A",{href:!0,target:!0});var A=L(t);x(e.$$.fragment,A),n=$(A,u[0]),A.forEach(v),f=j(c),a&&a.l(c),c.forEach(v),this.h()},h(){p(t,"href",i=u[3].html_url),p(t,"target","_blank"),p(r,"class","bg-indigo-100 dark:bg-gray-700")},m(l,c){T(l,r,c),o(r,t),M(e,t,null),o(t,n),o(r,f),a&&a.m(r,null),s=!0},p(l,c){(!s||c&1)&&me(n,l[0]),(!s||c&8&&i!==(i=l[3].html_url))&&p(t,"href",i),l[1]&&l[3].stargazers_count>0?a?(a.p(l,c),c&10&&b(a,1)):(a=se(l),a.c(),b(a,1),a.m(r,null)):a&&(fe(),I(a,1,1,()=>{a=null}),ue())},i(l){s||(b(e.$$.fragment,l),b(a),s=!0)},o(l){I(e.$$.fragment,l),I(a),s=!1},d(l){l&&v(r),G(e),a&&a.d()}}}function se(u){let r,t,e=u[3].stargazers_count+"",n,i,f;return t=new he({props:{icon:de,class:"inline",color:"#ddb14e"}}),{c(){r=d("a"),z(t.$$.fragment),n=y(e),this.h()},l(s){r=_(s,"A",{href:!0,target:!0,class:!0});var a=L(r);x(t.$$.fragment,a),n=$(a,e),a.forEach(v),this.h()},h(){p(r,"href",i=`${u[3].html_url}/stargazers`),p(r,"target","_blank"),p(r,"class","noblue no-underline")},m(s,a){T(s,r,a),M(t,r,null),o(r,n),f=!0},p(s,a){(!f||a&8)&&e!==(e=s[3].stargazers_count+"")&&me(n,e),(!f||a&8&&i!==(i=`${s[3].html_url}/stargazers`))&&p(r,"href",i)},i(s){f||(b(t.$$.fragment,s),f=!0)},o(s){I(t.$$.fragment,s),f=!1},d(s){s&&v(r),G(t)}}}function ve(u){let r,t,e=u[2]&&ne(u);return{c(){e&&e.c(),r=re()},l(n){e&&e.l(n),r=re()},m(n,i){e&&e.m(n,i),T(n,r,i),t=!0},p(n,[i]){n[2]?e?(e.p(n,i),i&4&&b(e,1)):(e=ne(n),e.c(),b(e,1),e.m(r.parentNode,r)):e&&(fe(),I(e,1,1,()=>{e=null}),ue())},i(n){t||(b(e),t=!0)},o(n){I(e),t=!1},d(n){n&&v(r),e&&e.d(n)}}}function be(u,r,t){let{owner:e="Vonr"}=r,{repo:n}=r,{showStars:i=!0}=r,f=!1,s;return ce(async()=>{t(3,s=await fetch(`https://api.github.com/repos/${e}/${n}`,{headers:{Accept:"application/vnd.github+json","X-GitHub-Api-Version":"2022-11-28"}}).then(a=>a.json()).then(a=>a)),t(2,f=!0)}),u.$$set=a=>{"owner"in a&&t(4,e=a.owner),"repo"in a&&t(0,n=a.repo),"showStars"in a&&t(1,i=a.showStars)},[n,i,f,s,e]}class q extends le{constructor(r){super(),ie(this,r,be,ve,oe,{owner:4,repo:0,showStars:1})}}function ye(u){let r,t,e,n="Portfolio",i,f,s=`Hey, I&#39;m Qther.
+        <br/>
+        Of course, that&#39;s just an alias as I choose not to reveal my real name for the sake of privacy.
+        <br/>`,a,l,c=`I started programming with Javascript when I was just 9 years old,
+        <br/>
+        though I did not make anything of note until much later.`,A,h,B,N,F,Z,D,H,J,O,Q,C,X,K,w,W,R,ae="configuration files",Y,P,ee,U;return N=new q({props:{repo:"parui"}}),H=new q({props:{repo:"compact_strings"}}),C=new q({props:{repo:"nonn"}}),P=new q({props:{repo:"align.nvim"}}),{c(){r=E(),t=d("div"),e=d("h1"),e.textContent=n,i=E(),f=d("p"),f.innerHTML=s,a=E(),l=d("p"),l.innerHTML=c,A=E(),h=d("p"),B=y(`Nowadays, I mostly program in Rust, as I am a big fan of its safety and
+        ergonomics. Apart from binaries like
+        `),z(N.$$.fragment),F=y(`, a terminal user interface wrapper around the Arch
+        User Repository (AUR) Helper paru,
+        `),Z=d("br"),D=y(`
+        I have also made several libraries such
+        `),z(H.$$.fragment),J=y(`
+        , a more space-efficient and cache-friendly way of representing large lists
+        of strings also used in parui,
+        `),O=d("br"),Q=y(`
+        and
+        `),z(C.$$.fragment),X=y(`, a wrapper around the standard library's NonZero*
+        integer types to make them generically NonN instead.`),K=E(),w=d("p"),W=y(`I use Neovim as my text editor, both for simple files and for
+        programming. I host my `),R=d("a"),R.textContent=ae,Y=y(`
+        on GitHub and have made some plugins for it, such as
+        `),z(P.$$.fragment),ee=y(", though they are largely unmaintained."),this.h()},l(m){ge("svelte-wdlemp",document.head).forEach(v),r=j(m),t=_(m,"DIV",{class:!0});var k=L(t);e=_(k,"H1",{"data-svelte-h":!0}),V(e)!=="svelte-1iek1aq"&&(e.textContent=n),i=j(k),f=_(k,"P",{class:!0,"data-svelte-h":!0}),V(f)!=="svelte-1wxtfm"&&(f.innerHTML=s),a=j(k),l=_(k,"P",{class:!0,"data-svelte-h":!0}),V(l)!=="svelte-u0p5cf"&&(l.innerHTML=c),A=j(k),h=_(k,"P",{class:!0});var g=L(h);B=$(g,`Nowadays, I mostly program in Rust, as I am a big fan of its safety and
+        ergonomics. Apart from binaries like
+        `),x(N.$$.fragment,g),F=$(g,`, a terminal user interface wrapper around the Arch
+        User Repository (AUR) Helper paru,
+        `),Z=_(g,"BR",{}),D=$(g,`
+        I have also made several libraries such
+        `),x(H.$$.fragment,g),J=$(g,`
+        , a more space-efficient and cache-friendly way of representing large lists
+        of strings also used in parui,
+        `),O=_(g,"BR",{}),Q=$(g,`
+        and
+        `),x(C.$$.fragment,g),X=$(g,`, a wrapper around the standard library's NonZero*
+        integer types to make them generically NonN instead.`),g.forEach(v),K=j(k),w=_(k,"P",{class:!0});var S=L(w);W=$(S,`I use Neovim as my text editor, both for simple files and for
+        programming. I host my `),R=_(S,"A",{href:!0,"data-svelte-h":!0}),V(R)!=="svelte-uv1yaj"&&(R.textContent=ae),Y=$(S,`
+        on GitHub and have made some plugins for it, such as
+        `),x(P.$$.fragment,S),ee=$(S,", though they are largely unmaintained."),S.forEach(v),k.forEach(v),this.h()},h(){document.title="Portfolio",p(f,"class","svelte-104u3se"),p(l,"class","svelte-104u3se"),p(h,"class","svelte-104u3se"),p(R,"href","https://github.com/Vonr/nvim"),p(w,"class","svelte-104u3se"),p(t,"class","centered-content")},m(m,te){T(m,r,te),T(m,t,te),o(t,e),o(t,i),o(t,f),o(t,a),o(t,l),o(t,A),o(t,h),o(h,B),M(N,h,null),o(h,F),o(h,Z),o(h,D),M(H,h,null),o(h,J),o(h,O),o(h,Q),M(C,h,null),o(h,X),o(t,K),o(t,w),o(w,W),o(w,R),o(w,Y),M(P,w,null),o(w,ee),U=!0},p:pe,i(m){U||(b(N.$$.fragment,m),b(H.$$.fragment,m),b(C.$$.fragment,m),b(P.$$.fragment,m),U=!0)},o(m){I(N.$$.fragment,m),I(H.$$.fragment,m),I(C.$$.fragment,m),I(P.$$.fragment,m),U=!1},d(m){m&&(v(r),v(t)),G(N),G(H),G(C),G(P)}}}class Ne extends le{constructor(r){super(),ie(this,r,null,ye,oe,{})}}export{Ne as component};
