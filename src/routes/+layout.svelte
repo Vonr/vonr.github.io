0057078670ede2aaa12398c125fe21457310b1e9
Vonr <dtaclanlegion@gmail.com>
2024-04-23 18:05:03 +0800

    Update dependencies and fix scrollbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -134,13 +134,7 @@
     </ul>
 </nav>
 
-<Svrollbar
-    alwaysVisible={true}
-    margin={{
-        top: 3.5 * rem,
-        buttom: 0.5 * rem,
-    }}
-/>
+<Svrollbar alwaysVisible={true} />
 
 <div class="fixed bottom-0 right-0 m-8">
     <a href="https://ko-fi.com/qther" target="_blank">
