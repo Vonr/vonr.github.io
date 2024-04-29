31aca0adf70d3a0dfd6f0716399e350d1075f14d
Vonr <Vonr@users.noreply.github.com>
2024-04-29 12:36:27 +0000

    deploy: 259468fd6d576fd6a4b74df8857e306521cc2fb3

--- _app/immutable/entry/start.C2hCYv9t.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.D51E8DyI.js";export{t as start};
---
+++ _app/immutable/nodes/5.CksUJ6GY.js
@@ -0,0 +1,29 @@
+import{s as oe,n as se}from"../chunks/scheduler.BfKkd2ys.js";import{S as ie,i as le,e as r,s as H,t as m,k as R,l as pe,c as o,d as l,f as N,a as E,m as T,b as f,n as M,o as A,h as e,g as ne,p as q,q as L,r as U,u as S}from"../chunks/index.C34Kbxwq.js";import{R as j}from"../chunks/Repo.ClXn2aDe.js";function me(re){let p,v,C,t,$,Y="Portfolio",G,_,ee=`Hey, I&#39;m Qther.
+        <br/>
+        That&#39;s an alias - as I choose not to reveal my real name for the sake of
+        privacy.
+        <br/>`,V,b,te=`I started programming with JavaScript when I was just 9 years old,
+        <br/>
+        though I did not make anything of note until much later.`,Z,h,z,d,D,J,s,Q,u,B,c,F,K,i,O,g,ae="configuration files",W,y,X,P;return d=new j({props:{repo:"parui"}}),u=new j({props:{repo:"compact_strings"}}),c=new j({props:{repo:"nonn"}}),y=new j({props:{repo:"align.nvim"}}),{c(){p=r("meta"),v=r("meta"),C=H(),t=r("div"),$=r("h1"),$.textContent=Y,G=H(),_=r("p"),_.innerHTML=ee,V=H(),b=r("p"),b.innerHTML=te,Z=H(),h=r("p"),z=m(`Nowadays, I mostly program in Rust, as I am a big fan of its safety and
+        ergonomics. Apart from binaries like
+        `),R(d.$$.fragment),D=m(`, a terminal user interface wrapper around the Arch
+        User Repository (AUR) Helper paru.`),J=H(),s=r("p"),Q=m(`I have also made several libraries such
+        `),R(u.$$.fragment),B=m(`
+        , a more space-efficient and cache-friendly way of representing large lists
+        of strings also used in parui, and
+        `),R(c.$$.fragment),F=m(`, a wrapper around the standard library's NonZero*
+        integer types to make them generically NonN instead.`),K=H(),i=r("p"),O=m(`I use Neovim as my text editor, both for simple files and for
+        programming. I host my `),g=r("a"),g.textContent=ae,W=m(`
+        on GitHub and have made some plugins for it, such as
+        `),R(y.$$.fragment),X=m(", though they are largely unmaintained."),this.h()},l(a){const w=pe("svelte-11qzclm",document.head);p=o(w,"META",{content:!0,property:!0}),v=o(w,"META",{content:!0,property:!0}),w.forEach(l),C=N(a),t=o(a,"DIV",{class:!0});var n=E(t);$=o(n,"H1",{"data-svelte-h":!0}),T($)!=="svelte-1iek1aq"&&($.textContent=Y),G=N(n),_=o(n,"P",{"data-svelte-h":!0}),T(_)!=="svelte-1fh79p9"&&(_.innerHTML=ee),V=N(n),b=o(n,"P",{"data-svelte-h":!0}),T(b)!=="svelte-qthw6n"&&(b.innerHTML=te),Z=N(n),h=o(n,"P",{});var k=E(h);z=f(k,`Nowadays, I mostly program in Rust, as I am a big fan of its safety and
+        ergonomics. Apart from binaries like
+        `),M(d.$$.fragment,k),D=f(k,`, a terminal user interface wrapper around the Arch
+        User Repository (AUR) Helper paru.`),k.forEach(l),J=N(n),s=o(n,"P",{});var I=E(s);Q=f(I,`I have also made several libraries such
+        `),M(u.$$.fragment,I),B=f(I,`
+        , a more space-efficient and cache-friendly way of representing large lists
+        of strings also used in parui, and
+        `),M(c.$$.fragment,I),F=f(I,`, a wrapper around the standard library's NonZero*
+        integer types to make them generically NonN instead.`),I.forEach(l),K=N(n),i=o(n,"P",{});var x=E(i);O=f(x,`I use Neovim as my text editor, both for simple files and for
+        programming. I host my `),g=o(x,"A",{href:!0,"data-svelte-h":!0}),T(g)!=="svelte-uv1yaj"&&(g.textContent=ae),W=f(x,`
+        on GitHub and have made some plugins for it, such as
+        `),M(y.$$.fragment,x),X=f(x,", though they are largely unmaintained."),x.forEach(l),n.forEach(l),this.h()},h(){document.title="Portfolio",A(p,"content","Portfolio"),A(p,"property","og:title"),A(v,"content","https://vonr.github.io/portfolio"),A(v,"property","og:url"),A(g,"href","https://github.com/Vonr/nvim"),A(t,"class","centered-content")},m(a,w){e(document.head,p),e(document.head,v),ne(a,C,w),ne(a,t,w),e(t,$),e(t,G),e(t,_),e(t,V),e(t,b),e(t,Z),e(t,h),e(h,z),q(d,h,null),e(h,D),e(t,J),e(t,s),e(s,Q),q(u,s,null),e(s,B),q(c,s,null),e(s,F),e(t,K),e(t,i),e(i,O),e(i,g),e(i,W),q(y,i,null),e(i,X),P=!0},p:se,i(a){P||(L(d.$$.fragment,a),L(u.$$.fragment,a),L(c.$$.fragment,a),L(y.$$.fragment,a),P=!0)},o(a){U(d.$$.fragment,a),U(u.$$.fragment,a),U(c.$$.fragment,a),U(y.$$.fragment,a),P=!1},d(a){a&&(l(C),l(t)),l(p),l(v),S(d),S(u),S(c),S(y)}}}class ue extends ie{constructor(p){super(),le(this,p,null,me,oe,{})}}export{ue as component};
